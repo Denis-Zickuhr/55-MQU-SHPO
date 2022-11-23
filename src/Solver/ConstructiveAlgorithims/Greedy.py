5d0be3e8389e095c70ffd6cb643650dee8a592d2
Denis Zickuhr <deniszickuhr@gmail.com>
2022-11-23 14:14:07 -0300

    add Utils

--- src/Solver/ConstructiveAlgorithims/Greedy.py
+++ src/Solver/ConstructiveAlgorithims/Greedy.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('55-MQU-SHPO/src/Utils')
 from loader import _LOAD
 
+
+
 (k, d, c, P, D, Depot) = _LOAD('55-MQU-SHPO\instances\A-n32-k5.txt')
 
 print(k, d, c, P, D, Depot)
