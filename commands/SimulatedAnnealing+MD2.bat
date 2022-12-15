672473234f3e3d84dc053a598e8814fc8cecf5e0
Denis Zickuhr <deniszickuhr@gmail.com>
2022-12-15 10:04:51 -0300

    fixed typo

--- commands/SimulatedAnnealing+MD2.bat
+++ commands/SimulatedAnnealing+MD2.bat
@@ -1,52 +1,52 @@
 @echo off
 
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n33-k5
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n37-k5
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n39-k5
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n44-k6
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n46-k7
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n54-k7
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n61-k9
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n63-k9
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n64-k9
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
-pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 360 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
+pypy src\Solver\GRASP\SimulatedAnnealingMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.26 --repeat 10000 --seconds 20 --trace yes --verbose no --totaltime 300 --temperature 78
 @echo -----n80-k10
