58ee27e6b0f691e3f0e976b669daf98dcdf28e13
Denis Zickuhr <deniszickuhr@gmail.com>
2022-12-15 10:02:33 -0300

    rafactored commands for running all instances

--- commands/TabooSearch+MD2.bat
+++ commands/TabooSearch+MD2.bat
@@ -1,3 +1,52 @@
 @echo off
 
-pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n80-k10.vrp --seconds 20 --alpha 0.29 --repeat 100 --trace yes --verbose no
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n33-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+@echo -----n33-k5
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n37-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+@echo -----n37-k5
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n39-k5.vrp --alpha 0.29 --repeat 10000 --seconds 5 --trace yes --verbose no --totaltime 300
+@echo -----n39-k5
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.29 --repeat 10000 --seconds 6 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.29 --repeat 10000 --seconds 6 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.29 --repeat 10000 --seconds 6 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n44-k6.vrp --alpha 0.29 --repeat 10000 --seconds 6 --trace yes --verbose no --totaltime 300
+@echo -----n44-k6
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n46-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+@echo -----n46-k7
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n54-k7.vrp --alpha 0.29 --repeat 10000 --seconds 7 --trace yes --verbose no --totaltime 300
+@echo -----n54-k7
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n61-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+@echo -----n61-k9
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n63-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+@echo -----n63-k9
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n64-k9.vrp --alpha 0.29 --repeat 10000 --seconds 9 --trace yes --verbose no --totaltime 300
+@echo -----n64-k9
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.29 --repeat 10000 --seconds 10 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.29 --repeat 10000 --seconds 10 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.29 --repeat 10000 --seconds 10 --trace yes --verbose no --totaltime 300
+pypy src\Solver\GRASP\TabooSearchMD2.py --ins instances/A/A-n80-k10.vrp --alpha 0.29 --repeat 10000 --seconds 10 --trace yes --verbose no --totaltime 300
+@echo -----n80-k10
